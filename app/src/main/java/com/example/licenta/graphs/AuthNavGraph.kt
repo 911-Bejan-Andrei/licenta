b12d3e0b0d7f8d7b8a65f17e6631148478bfa294
911-Bejan-Andrei <andrei.bejan1@stud.ubbcluj.ro>
2024-03-21 17:30:32 +0200

    navigation2

--- app/src/main/java/com/example/licenta/graphs/AuthNavGraph.kt
+++ app/src/main/java/com/example/licenta/graphs/AuthNavGraph.kt
@@ -4,26 +4,22 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.example.licenta.constants.AuthenticationScreen
 import com.example.licenta.constants.Graph
 import com.example.licenta.screens.auth.LoginScreen
 
 fun NavGraphBuilder.authNavGraph(navController: NavHostController) {
     navigation(
         route = Graph.AUTHENTICATION,
-        startDestination = AuthScreen.Login.route
+        startDestination = AuthenticationScreen.Login.route
     ) {
-        composable(route = AuthScreen.Login.route) {
+        composable(route = AuthenticationScreen.Login.route) {
             LoginScreen(
                 onClick = {
                     navController.popBackStack()
                     navController.navigate(Graph.MAIN)
                 },
-
             )
         }
     }
-}
-
-sealed class AuthScreen(val route: String) {
-    object Login : AuthScreen(route = "LOGIN")
 }
